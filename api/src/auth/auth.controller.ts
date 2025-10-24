80708d3fc53c0ae5a32da20ae102648df5f5ca5a
yulan <yulan.nguyenvan@ynov.com>
2025-10-24 18:12:42 +0200

    refactor(auth): convert promise-based methods to async/await for improved readability

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -11,7 +11,7 @@ import { Throttle } from '@nestjs/throttler';
 @ApiTags('auth')
 @Controller('api/v1/auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
 
   @UseGuards(AuthGuard('local'))
   @Throttle({ default: { limit: 3, ttl: 60000 } })
@@ -19,35 +19,31 @@ export class AuthController {
   @ApiOperation({ summary: 'Se connecter avec e-mail et mot de passe' })
   @ApiBody({ type: SignInDto, description: 'Informations de connexion' })
   @ApiResponse({ status: 200, description: 'Connexion réussie', type: AuthResultDto })
-  login(@Request() req, @Res({ passthrough: true }) res: Response) {
-    return this.authService.signIn(req.user).then(result => {
-      res.cookie('refreshToken', result.refreshToken, {
-        httpOnly: true,
-        secure: true,
-        sameSite: 'strict',
-        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 jours
-      });
-
-      return { accessToken: result.accessToken, user: result.user };
+  async login(@Request() req, @Res({ passthrough: true }) res: Response) {
+    const result = await this.authService.signIn(req.user);
+    res.cookie('refreshToken', result.refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 jours
     });
+    return { accessToken: result.accessToken, user: result.user };
   }
 
-  @Post('register')
   @Throttle({ default: { limit: 3, ttl: 60000 } })
+  @Post('register')
   @ApiOperation({ summary: 'Créer un nouvel utilisateur' })
   @ApiBody({ type: SignUpDto, description: 'Informations pour l’inscription' })
   @ApiResponse({ status: 201, description: 'Utilisateur créé avec succès', type: AuthResultDto })
-  register(@Body() signUpDto: SignUpDto, @Res({ passthrough: true }) res: Response) {
-    return this.authService.register(signUpDto).then(result => {
-      res.cookie('refreshToken', result.refreshToken, {
-        httpOnly: true,
-        secure: true,
-        sameSite: 'strict',
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-      });
-
-      return { accessToken: result.accessToken, user: result.user };
+  async register(@Body() signUpDto: SignUpDto, @Res({ passthrough: true }) res: Response) {
+    const result = await this.authService.register(signUpDto);
+    res.cookie('refreshToken', result.refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     });
+    return { accessToken: result.accessToken, user: result.user };
   }
 
   @UseGuards(AuthGuard('jwt'))
@@ -59,7 +55,6 @@ export class AuthController {
     return { user: req.user };
   }
 
-
   @Throttle({ default: { limit: 5, ttl: 60000 } })
   @Post('refresh')
   @ApiOperation({ summary: 'Rafraîchir le token d’accès' })
@@ -70,18 +65,18 @@ export class AuthController {
       example: { accessToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...' },
     },
   })
-  refresh(@Request() req, @Res({ passthrough: true }) res: Response) {
+  async refresh(@Request() req, @Res({ passthrough: true }) res: Response) {
     const refreshToken = req.cookies['refreshToken'];
-    return this.authService.refreshToken(refreshToken).then(({ accessToken, refreshToken: newRefreshToken }) => {
-      res.cookie('refreshToken', newRefreshToken, {
-        httpOnly: true,
-        secure: true,
-        sameSite: 'strict',
-        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 jours
-      });
+    const { accessToken, refreshToken: newRefreshToken } = await this.authService.refreshToken(refreshToken);
 
-      return { accessToken };
+    res.cookie('refreshToken', newRefreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 jours
     });
+
+    return { accessToken };
   }
 
   @UseGuards(AuthGuard('jwt'))
@@ -94,10 +89,9 @@ export class AuthController {
     description: 'Déconnexion réussie',
     schema: { example: { message: 'Logged out successfully' } },
   })
-  logout(@Request() req, @Res({ passthrough: true }) res: Response) {
-    return this.authService.logout(req.user.sub).then(() => {
-      res.clearCookie('refreshToken');
-      return { message: 'Logged out successfully' };
-    });
+  async logout(@Request() req, @Res({ passthrough: true }) res: Response) {
+    await this.authService.logout(req.user.sub);
+    res.clearCookie('refreshToken');
+    return { message: 'Logged out successfully' };
   }
 }
