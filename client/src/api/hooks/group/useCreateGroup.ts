9d7fb4491745b0b13af0985297d674aafac35abf
yulannn <145021271+yulannn@users.noreply.github.com>
2025-11-29 15:06:32 +0100

    feat: Add `useCreateGroup` hook for creating new groups.

--- client/src/api/hooks/group/useCreateGroup.ts
+++ client/src/api/hooks/group/useCreateGroup.ts
@@ -5,17 +5,18 @@ import type { Group, CreateGroupPayload } from '../../../types/group.type';
 export function useCreateGroup(options?: Partial<UseMutationOptions<Group, unknown, CreateGroupPayload>>) {
   const qc = useQueryClient();
 
+  const { onSuccess, ...otherOptions } = options || {};
+
   const mutation = useMutation<Group, unknown, CreateGroupPayload>({
     mutationFn: (payload: CreateGroupPayload) => GroupService.createGroup(payload),
     onSuccess: async (...args) => {
       await qc.invalidateQueries({ queryKey: ['groups'] });
-      await qc.invalidateQueries({ queryKey: ['myGroups'] });
       await qc.invalidateQueries({ queryKey: ['conversations'] });
-      if (options?.onSuccess) {
-        options.onSuccess(...args);
+      if (onSuccess) {
+        onSuccess(...args);
       }
     },
-    ...options,
+    ...otherOptions,
   });
   return mutation;
 }
