86cca634ed54a48b007cd4bc8477efb317b7a2ca
yulannn <145021271+yulannn@users.noreply.github.com>
2025-12-05 23:01:51 +0100

    feat: implement GymMap component to display nearby gyms with map/list views and search functionality based on user's city.

--- client/src/components/home/GymMap.tsx
+++ client/src/components/home/GymMap.tsx
@@ -421,8 +421,8 @@ export default function GymMap() {
             scrollWheelZoom={false}
           >
             <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+              url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
             />
 
             {displayedGyms.map((gym) => (
